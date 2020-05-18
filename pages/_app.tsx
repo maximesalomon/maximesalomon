232e692ce32af14dfd9b3327b7c66ff2cb7d2694
Maxime Salomon <maxime@croissant.io>
2020-05-18 22:38:17 +0200

    MAXIME SALOMON APP comment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app'
 import '../css/index.css'
 
+// MAXIME SALOMON APP
 const MaximeSalomon = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
