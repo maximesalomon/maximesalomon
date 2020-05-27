06221d0670d6f7405d1e676cb194387804b3716a
Maxime Salomon <maxime@croissant.io>
2020-05-27 22:52:45 +0200

    maxime salomon

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,7 +6,7 @@ import Layout from '../components/Layout'
 const AboutPage: React.FunctionComponent = () => (
   <Layout title="About">
     <h1>About</h1>
-    <p>This is the about page</p>
+    <p>Maxime Salomon</p>
     <p>
       <Link href="/">
         <a>Go home</a>
