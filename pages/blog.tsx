a905e8329381ef32f8d1302cb7b7cb23e0e0eb2d
Maxime Salomon <maxime@croissant.io>
2020-06-04 08:02:15 +0200

    rm HeroPost

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -2,23 +2,15 @@ import React from "react";
 import Layout from "../components/Layout";
 import { getAllPosts } from "../lib/api";
 import Head from "next/head";
-import HeroPost from "../components/HeroPost";
 
 // BLOG PAGE
 const BlogPage: React.FunctionComponent = ({ allPosts }: any) => {
-  const heroPost = allPosts[0];
-
   return (
+    
     <Layout title="Blog">
       <Head>
         <title>Blog</title>
       </Head>
-      {heroPost && (
-        <HeroPost
-          title={heroPost.title}
-          slug={heroPost.slug}
-        />
-      )}
     </Layout>
   );
 };
