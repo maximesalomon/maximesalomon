d50054d167d02b83af9d90799cf56c8f069ba1c9
Maxime Salomon <maxime@croissant.io>
2020-06-07 10:38:09 +0200

    dynamically create staticPaths

--- pages/[post].tsx
+++ pages/[post].tsx
@@ -1,13 +1,14 @@
 import Layout from "../components/Layout";
+import fs from "fs";
 import { getAllPosts } from "../lib/api";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
 // ARTICLE PAGE
 const ArticlePage: React.FunctionComponent = ({ posts }: any) => {
-  const router = useRouter()
+  const router = useRouter();
   const slug = router.query.post;
-  const filteredPost = posts.filter((post: any) => post.slug === slug)
-  const post = filteredPost[0]
+  const filteredPost = posts.filter((post: any) => post.slug === slug);
+  const post = filteredPost[0];
 
   return (
     <Layout title="Blog">
@@ -24,23 +25,26 @@ export default ArticlePage;
 // TODO https://www.youtube.com/watch?v=pY0vWYLDDco
 
 export const getStaticPaths = async () => {
+  const files = fs.readdirSync("posts");
+  console.log('files', files);
+  const paths = files.map((filename) => ({
+    params: {
+      post: filename.replace(".md", ""),
+    },
+  }));
+  console.log('paths :', paths)
+  
   return {
-    paths: [
-      { params: { post: 'from-marketing-to-growth-engineering-part-1' } },
-      { params: { post: 'from-marketing-to-growth-engineering-part-2' } },
-    ],
+    paths,
     fallback: false,
   };
-}
+};
+
 
 export const getStaticProps = async () => {
-  const posts = getAllPosts([
-    "title",
-    "date",
-    "slug",
-  ]);
+  const posts = getAllPosts(["title", "date", "slug"]);
 
   return {
     props: { posts },
   };
-}
+};
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -13,7 +13,7 @@ const BlogPage: React.FunctionComponent = ({ posts }: any) => {
       <h1 className="mt-8 text-3xl">Blog</h1>
       { posts.map((post: any) => {
         return (
-          <div className="mt-8">
+          <div key={post.slug}className="mt-8">
             <h3 className="text-2xl text-blue-800"><a href={post.slug}>{post.title}</a></h3>
             <p className="text-sm italic">{post.date}</p>
           </div>
