d7938104a5a090bde06c92d1c1a466a54f660590
Maxime Salomon <maxime@croissant.io>
2020-07-12 13:13:53 +0200

    rm 27 about

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,7 +7,6 @@ const AboutPage: React.FunctionComponent = () => (
   <Layout title="About">
     <h1>About</h1>
     <p>Maxime Salomon</p>
-    <p>27</p>
     <p>
       <Link href="/">
         <a>Home</a>
