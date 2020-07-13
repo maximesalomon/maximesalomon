8d6dc55c390d9db7912098a8c686e951956dc80c
Maxime Salomon <maxime@croissant.io>
2020-07-13 19:30:26 +0200

    Document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-// NEXT DOCUMENT
+// DOCUMENT
 export default class MyDocument extends Document {
   render() {
     return (
