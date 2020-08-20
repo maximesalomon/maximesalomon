2273faeb6a95c1c4b6f010bbe91ed1a47e5a66e6
Maxime Salomon <maxime@croissant.io>
2020-08-20 09:50:35 +0200

    wip

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,7 +9,7 @@ const About: React.FunctionComponent = () => (
     <p>Maxime Salomon 👨‍💻</p>
     <p>27 🚣</p>
     <p>Aix-en-Provence, France 🌻</p>
-    {/* <p>Founder, Croissant 🥐</p> */}
+    <p>Founder, Croissant 🥐</p>
     <p>
       <Link href="/">
         <a>Home</a>
